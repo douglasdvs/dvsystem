cf0e1f67cd0e328dec97714cf94f98c87c1ce90c
dvsdougao <mvprint.personalizados@gmail.com>
2025-06-24 23:38:26 -0300

    Update settings.py

--- dvsystem/settings.py
+++ dvsystem/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 
