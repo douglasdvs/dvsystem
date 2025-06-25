4ac6939bda5ba96a344417f240fc43030170d7d8
dvsdougao <mvprint.personalizados@gmail.com>
2025-06-25 13:30:29 -0300

    Update wsgi.py

--- dvsystem/wsgi.py
+++ dvsystem/wsgi.py
@@ -1,4 +1,8 @@
-import os
-from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dvsystem.settings')
-application = get_wsgi_application()
+import sys
+try:
+    # ...existing code...
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+except Exception as e:
+    print("WSGI ERROR:", e, file=sys.stderr)
+    raise
