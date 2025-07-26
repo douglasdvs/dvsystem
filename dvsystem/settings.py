fe28d4169dd4384deb7b2bd011ac45b9a3a09700
douglasdvs <douglasd5@hotmail.com>
2025-07-26 00:16:53 -0300

    Update settings.py

--- dvsystem/settings.py
+++ dvsystem/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv())
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,supabase-py.onrender.com").split(",")
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = reverse_lazy("core:index")
